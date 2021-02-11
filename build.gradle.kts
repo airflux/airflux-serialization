7b9dab7e09f9c2fd737ac081fe03933e168914ac
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-02-11 20:39:05 +0200

    Changed the Gradle files.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
-tasks.register("cleanAll") {
+tasks.register("clean") {
     dependsOn(gradle.includedBuild("core").task(":clean"))
     dependsOn(gradle.includedBuild("dsl").task(":clean"))
     dependsOn(gradle.includedBuild("jackson-parser").task(":clean"))
 }
 
-tasks.register("buildAll") {
+tasks.register("build") {
     dependsOn(gradle.includedBuild("core").task(":build"))
     dependsOn(gradle.includedBuild("dsl").task(":build"))
     dependsOn(gradle.includedBuild("jackson-parser").task(":build"))
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,5 @@
 rootProject.name = "airflux"
 
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-    }
-}
-
 includeBuild("core")
 includeBuild("dsl")
 includeBuild("jackson-parser")
