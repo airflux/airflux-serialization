e64ae755af864adcdd83e96fa6916413ce99555d
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-02-26 11:15:35 +0200

    Added new functions for reading optional fields.

---
+++ airflux-core/src/main/kotlin/io/github/airflux/reader/OptionalWithDefaultFieldReader.kt
@@ -0,0 +1,65 @@
+package io.github.airflux.reader
+
+import io.github.airflux.lookup.JsLookup
+import io.github.airflux.path.JsPath
+import io.github.airflux.reader.result.JsError
+import io.github.airflux.reader.result.JsResult
+import io.github.airflux.value.JsValue
+import io.github.airflux.value.extension.lookup
+
+fun <T : Any> readOptionalWithDefault(
+    from: JsLookup,
+    using: JsReader<T>,
+    defaultValue: () -> T,
+    invalidTypeErrorBuilder: (expected: JsValue.Type, actual: JsValue.Type) -> JsError
+): JsResult<T?> =
+    when (from) {
+        is JsLookup.Defined -> using.read(from.value).repath(from.path)
+
+        is JsLookup.Undefined.PathMissing -> JsResult.Success(path = from.path, value = defaultValue())
+
+        is JsLookup.Undefined.InvalidType ->
+            JsResult.Failure(path = from.path, error = invalidTypeErrorBuilder(from.expected, from.actual))
+    }
+
+/**
+ * Reads nullable field at [path].
+ *
+ * - If any node in [path] is not found then returns 'null'
+ * - If any node does not match path element type, then returning error [invalidTypeErrorBuilder]
+ * - If the entire path is found then applies [reader]
+ */
+fun <T : Any> readOptionalWithDefault(
+    from: JsValue,
+    path: JsPath,
+    using: JsReader<T>,
+    defaultValue: () -> T,
+    invalidTypeErrorBuilder: (expected: JsValue.Type, actual: JsValue.Type) -> JsError
+): JsResult<T?> =
+    readOptionalWithDefault(
+        from = from.lookup(path),
+        using = using,
+        defaultValue = defaultValue,
+        invalidTypeErrorBuilder = invalidTypeErrorBuilder
+    )
+
+/**
+ * Reads nullable field by [name].
+ *
+ * - If node is not found then returns 'null'
+ * - If node is not object, then returning error [invalidTypeErrorBuilder]
+ * - If the entire path is found then applies [reader]
+ */
+fun <T : Any> readOptionalWithDefault(
+    from: JsValue,
+    name: String,
+    using: JsReader<T>,
+    defaultValue: () -> T,
+    invalidTypeErrorBuilder: (expected: JsValue.Type, actual: JsValue.Type) -> JsError
+): JsResult<T?> =
+    readOptionalWithDefault(
+        from = from.lookup(name),
+        using = using,
+        defaultValue = defaultValue,
+        invalidTypeErrorBuilder = invalidTypeErrorBuilder
+    )
---
+++ airflux-core/src/test/kotlin/io/github/airflux/reader/OptionalWithDefaultFieldReaderTest.kt
@@ -0,0 +1,316 @@
+package io.github.airflux.reader
+
+import io.github.airflux.common.JsonErrors
+import io.github.airflux.common.TestData.DEFAULT_VALUE
+import io.github.airflux.common.TestData.USER_NAME_VALUE
+import io.github.airflux.lookup.JsLookup
+import io.github.airflux.path.JsPath
+import io.github.airflux.reader.result.JsResult
+import io.github.airflux.value.JsNull
+import io.github.airflux.value.JsObject
+import io.github.airflux.value.JsString
+import io.github.airflux.value.JsValue
+import org.junit.jupiter.api.Nested
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class OptionalWithDefaultFieldReaderTest {
+
+    companion object {
+        private val stringReader: JsReader<String> =
+            JsReader { input ->
+                when (input) {
+                    is JsString -> JsResult.Success(input.underlying)
+                    else -> JsResult.Failure(
+                        JsonErrors.InvalidType(expected = JsValue.Type.STRING, actual = input.type)
+                    )
+                }
+            }
+        private val defaultValue = { DEFAULT_VALUE }
+    }
+
+    @Nested
+    inner class FromJsLookup {
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is found)`() {
+            val from: JsLookup = JsLookup.Defined(path = JsPath.empty / "name", JsString(USER_NAME_VALUE))
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = from,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Success
+            assertEquals(JsPath.empty / "name", result.path)
+            assertEquals(USER_NAME_VALUE, result.value)
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is found with value 'null')`() {
+            val from: JsLookup = JsLookup.Defined(path = JsPath.empty / "name", JsNull)
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = from,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Failure
+            assertEquals(1, result.errors.size)
+
+            result.errors[0]
+                .also { (pathError, errors) ->
+                    assertEquals(JsPath.empty / "name", pathError)
+
+                    assertEquals(1, errors.size)
+                    val error = errors[0] as JsonErrors.InvalidType
+                    assertEquals(JsValue.Type.STRING, error.expected)
+                    assertEquals(JsValue.Type.NULL, error.actual)
+                }
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is not found, returning default value)`() {
+            val from: JsLookup = JsLookup.Undefined.PathMissing(path = JsPath.empty / "name")
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = from,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Success
+            assertEquals(JsPath.empty / "name", result.path)
+            assertEquals(DEFAULT_VALUE, result.value)
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is not found, invalid type)`() {
+            val from: JsLookup = JsLookup.Undefined.InvalidType(
+                path = JsPath.empty / "name",
+                expected = JsValue.Type.ARRAY,
+                actual = JsValue.Type.STRING
+            )
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = from,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Failure
+            assertEquals(1, result.errors.size)
+            result.errors[0]
+                .also { (pathError, errors) ->
+                    assertEquals(JsPath.empty / "name", pathError)
+
+                    assertEquals(1, errors.size)
+                    val error = errors[0] as JsonErrors.InvalidType
+                    assertEquals(JsValue.Type.ARRAY, error.expected)
+                    assertEquals(JsValue.Type.STRING, error.actual)
+                }
+        }
+    }
+
+    @Nested
+    inner class FromJsValueByPath {
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is found)`() {
+            val json: JsValue = JsObject(
+                "name" to JsString(USER_NAME_VALUE)
+            )
+            val path = JsPath.empty / "name"
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                path = path,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Success
+            assertEquals(JsPath.empty / "name", result.path)
+            assertEquals(USER_NAME_VALUE, result.value)
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is found with value 'null')`() {
+            val json: JsValue = JsObject(
+                "name" to JsNull
+            )
+            val path = JsPath.empty / "name"
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                path = path,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Failure
+            assertEquals(1, result.errors.size)
+
+            result.errors[0]
+                .also { (pathError, errors) ->
+                    assertEquals(JsPath.empty / "name", pathError)
+
+                    assertEquals(1, errors.size)
+                    val error = errors[0] as JsonErrors.InvalidType
+                    assertEquals(JsValue.Type.STRING, error.expected)
+                    assertEquals(JsValue.Type.NULL, error.actual)
+                }
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is not found, returning default value)`() {
+            val json: JsValue = JsObject(
+                "name" to JsString(USER_NAME_VALUE)
+            )
+            val path = JsPath.empty / "role"
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                path = path,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Success
+            assertEquals(JsPath.empty / "role", result.path)
+            assertEquals(DEFAULT_VALUE, result.value)
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is not found, invalid type)`() {
+            val json: JsValue = JsString(USER_NAME_VALUE)
+            val path = JsPath.empty / "name"
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                path = path,
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Failure
+            assertEquals(1, result.errors.size)
+
+            result.errors[0]
+                .also { (pathError, errors) ->
+                    assertEquals(JsPath.empty, pathError)
+
+                    assertEquals(1, errors.size)
+                    val error = errors[0] as JsonErrors.InvalidType
+                    assertEquals(JsValue.Type.OBJECT, error.expected)
+                    assertEquals(JsValue.Type.STRING, error.actual)
+                }
+        }
+    }
+
+    @Nested
+    inner class FromJsValueByName {
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is found)`() {
+            val json: JsValue = JsObject(
+                "name" to JsString(USER_NAME_VALUE)
+            )
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                name = "name",
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Success
+            assertEquals(JsPath.empty / "name", result.path)
+            assertEquals(USER_NAME_VALUE, result.value)
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is found with value 'null')`() {
+            val json: JsValue = JsObject(
+                "name" to JsNull
+            )
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                name = "name",
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Failure
+            assertEquals(1, result.errors.size)
+
+            result.errors[0]
+                .also { (pathError, errors) ->
+                    assertEquals(JsPath.empty / "name", pathError)
+
+                    assertEquals(1, errors.size)
+                    val error = errors[0] as JsonErrors.InvalidType
+                    assertEquals(JsValue.Type.STRING, error.expected)
+                    assertEquals(JsValue.Type.NULL, error.actual)
+                }
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is not found, returning default value)`() {
+            val json: JsValue = JsObject(
+                "name" to JsString(USER_NAME_VALUE)
+            )
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                name = "role",
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Success
+            assertEquals(JsPath.empty / "role", result.path)
+            assertEquals(DEFAULT_VALUE, result.value)
+        }
+
+        @Test
+        fun `Testing 'readOptionalWithDefault' function (an attribute is not found, invalid type)`() {
+            val json: JsValue = JsString(USER_NAME_VALUE)
+
+            val result: JsResult<String?> = readOptionalWithDefault(
+                from = json,
+                name = "name",
+                using = stringReader,
+                defaultValue = defaultValue,
+                invalidTypeErrorBuilder = JsonErrors::InvalidType
+            )
+
+            result as JsResult.Failure
+            assertEquals(1, result.errors.size)
+
+            result.errors[0]
+                .also { (pathError, errors) ->
+                    assertEquals(JsPath.empty, pathError)
+
+                    assertEquals(1, errors.size)
+                    val error = errors[0] as JsonErrors.InvalidType
+                    assertEquals(JsValue.Type.OBJECT, error.expected)
+                    assertEquals(JsValue.Type.STRING, error.actual)
+                }
+        }
+    }
+}
