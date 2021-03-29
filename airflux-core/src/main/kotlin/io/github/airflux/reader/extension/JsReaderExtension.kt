f6da9770e1f3ae8ed6b284f3e8e1820a309f8882
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-03-29 12:50:27 +0300

    Added new extension-function for reading 'JsValue' as 'JsObject'.

--- airflux-core/src/main/kotlin/io/github/airflux/reader/extension/JsReaderExtension.kt
+++ airflux-core/src/main/kotlin/io/github/airflux/reader/extension/JsReaderExtension.kt
@@ -4,6 +4,7 @@ import io.github.airflux.reader.result.JsError
 import io.github.airflux.reader.result.JsResult
 import io.github.airflux.value.JsBoolean
 import io.github.airflux.value.JsNumber
+import io.github.airflux.value.JsObject
 import io.github.airflux.value.JsString
 import io.github.airflux.value.JsValue
 
@@ -23,4 +24,12 @@ fun <T : Number> JsValue.readAsNumber(
 ) = when (this) {
     is JsNumber -> transformer(this.underlying)
     else -> JsResult.Failure(error = errorInvalidType(JsValue.Type.NUMBER, this.type))
+}
+
+fun<T> JsValue.readAsObject(
+    errorInvalidType: (expected: JsValue.Type, actual: JsValue.Type) -> JsError,
+    reader: (JsObject) -> JsResult<T>
+) = when (this) {
+    is JsObject -> reader(this)
+    else -> JsResult.Failure(error = errorInvalidType(JsValue.Type.OBJECT, this.type))
 }
