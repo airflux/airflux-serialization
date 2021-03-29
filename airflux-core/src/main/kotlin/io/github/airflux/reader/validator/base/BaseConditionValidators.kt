40461681f05f4e1a11a7c403fc0844ce5a97fa56
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-03-29 13:02:34 +0300

    Added new condition function for validators.

--- airflux-core/src/main/kotlin/io/github/airflux/reader/validator/base/BaseConditionValidators.kt
+++ airflux-core/src/main/kotlin/io/github/airflux/reader/validator/base/BaseConditionValidators.kt
@@ -4,6 +4,8 @@ import io.github.airflux.reader.result.JsError
 import io.github.airflux.reader.validator.JsValidationResult
 import io.github.airflux.reader.validator.JsValidator
 
+fun <T, E : JsError> applyIfPresent(validator: JsValidator<T, E>) = applyIfNotNull(validator)
+
 fun <T, E> applyIfNotNull(validator: JsValidator<T, E>)
     where E : JsError =
     JsValidator<T?, E> { value ->
