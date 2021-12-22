bd7ffd0fe87bf9883345d5326a4c6039cdef43f8
Maxim Sambulat <maxim.sambulat@gmail.com>
2021-12-22 13:19:31 +0200

    Refactored 'JsLookup' (moved extension-functions 'div' to 'JsLookup').

--- airflux-core/src/main/kotlin/io/github/airflux/lookup/JsLookup.kt
+++ airflux-core/src/main/kotlin/io/github/airflux/lookup/JsLookup.kt
@@ -1,15 +1,43 @@
 package io.github.airflux.lookup
 
 import io.github.airflux.reader.result.JsLocation
+import io.github.airflux.value.JsArray
+import io.github.airflux.value.JsObject
 import io.github.airflux.value.JsValue
 
+@Suppress("unused")
 sealed class JsLookup {
 
     abstract val location: JsLocation
 
-    data class Defined(override val location: JsLocation, val value: JsValue) : JsLookup()
+    abstract operator fun div(name: String): JsLookup
+
+    abstract operator fun div(idx: Int): JsLookup
+
+    data class Defined(override val location: JsLocation, val value: JsValue) : JsLookup() {
+
+        override fun div(name: String): JsLookup = when (value) {
+            is JsObject -> value[name]
+                ?.let { Defined(location = location / name, value = it) }
+                ?: Undefined.PathMissing(location = location / name)
+
+            else -> Undefined.InvalidType(location = location, expected = JsValue.Type.OBJECT, actual = value.type)
+        }
+
+        override fun div(idx: Int): JsLookup = when (value) {
+            is JsArray<*> -> value[idx]
+                ?.let { Defined(location = location / idx, value = it) }
+                ?: Undefined.PathMissing(location = location / idx)
+
+            else -> Undefined.InvalidType(location = location, expected = JsValue.Type.ARRAY, actual = value.type)
+        }
+    }
 
     sealed class Undefined : JsLookup() {
+
+        override fun div(name: String): JsLookup = this
+
+        override fun div(idx: Int): JsLookup = this
 
         data class PathMissing(override val location: JsLocation) : Undefined()
 
---
+++ airflux-core/src/test/kotlin/io/github/airflux/lookup/JsLookupFindByKeyTest.kt
@@ -0,0 +1,63 @@
+package io.github.airflux.lookup
+
+import io.github.airflux.common.TestData.USER_NAME_VALUE
+import io.github.airflux.reader.result.JsLocation
+import io.github.airflux.value.JsObject
+import io.github.airflux.value.JsString
+import io.github.airflux.value.JsValue
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class JsLookupFindByKeyTest {
+
+    @Test
+    fun `Element is found`() {
+        val defined: JsLookup = JsLookup.Defined(
+            location = JsLocation.Root / "user",
+            value = JsObject("name" to JsString(USER_NAME_VALUE))
+        )
+
+        val value = defined / "name"
+
+        value as JsLookup.Defined
+        assertEquals("#/user/name", value.location.toString())
+        assertEquals(USER_NAME_VALUE, (value.value as JsString).underlying)
+    }
+
+    @Test
+    fun `Element is not found`() {
+        val defined: JsLookup = JsLookup.Defined(
+            location = JsLocation.Root / "user",
+            value = JsObject("name" to JsString(USER_NAME_VALUE))
+        )
+
+        val value = defined / "type"
+
+        value as JsLookup.Undefined.PathMissing
+        assertEquals("#/user/type", value.location.toString())
+    }
+
+    @Test
+    fun `Element is invalid type`() {
+        val defined: JsLookup = JsLookup.Defined(
+            location = JsLocation.Root / "user",
+            value = JsObject("name" to JsString(USER_NAME_VALUE))
+        )
+
+        val value = defined / "name" / "id"
+
+        value as JsLookup.Undefined.InvalidType
+        assertEquals("#/user/name", value.location.toString())
+        assertEquals(JsValue.Type.OBJECT, value.expected)
+        assertEquals(JsValue.Type.STRING, value.actual)
+    }
+
+    @Test
+    fun `Element is undefined`() {
+        val undefined: JsLookup = JsLookup.Undefined.PathMissing(JsLocation.Root / "user")
+
+        val value = undefined / "phones"
+
+        assertEquals(undefined, value)
+    }
+}
