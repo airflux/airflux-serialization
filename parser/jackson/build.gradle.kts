58e57b0c42d3ca5ba4cfbefc7cdf96830d7d3185
Maxim Sambulat <maxim.sambulat@gmail.com>
2022-01-27 20:52:47 +0200

    Wrote test for the parser.

--- parser/jackson/build.gradle.kts
+++ parser/jackson/build.gradle.kts
@@ -24,15 +24,9 @@ dependencies {
         exclude(group = "org.jetbrains.kotlin")
     }
 
-    /* Test */
-    testImplementation(kotlin("test-junit5"))
-
-    /* Junit */
-    testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.Test.JUnit.Jupiter}")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:${Versions.Test.JUnit.Jupiter}")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:${Versions.Test.JUnit.Jupiter}")
-    testImplementation("org.junit.platform:junit-platform-engine:${Versions.Test.JUnit.Platform}")
-    testImplementation("org.junit.platform:junit-platform-launcher:${Versions.Test.JUnit.Platform}")
+    /* Kotest */
+    testImplementation("io.kotest:kotest-runner-junit5:${Versions.Test.Kotest}")
+    testImplementation("io.kotest:kotest-assertions-core:${Versions.Test.Kotest}")
 
     /* PITest */
     testImplementation("org.pitest:pitest-junit5-plugin:${Versions.Test.PiTest.JUnit5}")
--- parser/jackson/src/test/kotlin/io/github/airflux/AirFluxJsonModuleTest.kt
+++ parser/jackson/src/test/kotlin/io/github/airflux/AirFluxJsonModuleTest.kt
@@ -1,10 +1,139 @@
 package io.github.airflux
 
-import kotlin.test.Test
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.airflux.core.value.JsArray
+import io.github.airflux.core.value.JsBoolean
+import io.github.airflux.core.value.JsNull
+import io.github.airflux.core.value.JsNumber
+import io.github.airflux.core.value.JsObject
+import io.github.airflux.core.value.JsString
+import io.github.airflux.core.value.JsValue
+import io.github.airflux.parser.AirFluxJsonModule
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeInstanceOf
 
-class AirFluxJsonModuleTest {
+class AirFluxJsonModuleTest : FreeSpec() {
 
-    @Test
-    fun test() {
+    companion object {
+        private val mapper = ObjectMapper().apply {
+            registerModule(AirFluxJsonModule)
+        }
+        private fun String.deserialization(): JsValue = mapper.readValue(this, JsValue::class.java)
+        private fun JsValue.serialization(): String = mapper.writeValueAsString(this)
+    }
+
+    init {
+
+        "Deserialization" - {
+
+            "success" - {
+
+                "the value as the JsObject" - {
+
+                    "with the value as the JsNumber" {
+                        val json = """{"id": 123}""".deserialization()
+
+                        val root = json.shouldBeInstanceOf<JsObject>()
+                        val id = root["id"]
+                        val value = id.shouldBeInstanceOf<JsNumber>()
+                        value.get shouldBe "123"
+                    }
+
+                    "the value as the JsString" {
+                        val json = """{"name": "user-1"}""".deserialization()
+
+                        val root = json.shouldBeInstanceOf<JsObject>()
+                        val name = root["name"]
+                        val value = name.shouldBeInstanceOf<JsString>()
+                        value.get shouldBe "user-1"
+                    }
+
+                    "the value as the JsBoolean" - {
+
+                        "true" {
+                            val json = """{"isActive": true}""".deserialization()
+
+                            val root = json.shouldBeInstanceOf<JsObject>()
+                            val isActive = root["isActive"]
+                            isActive.shouldBeInstanceOf<JsBoolean.True>()
+                        }
+
+                        "false" {
+                            val json = """{"isActive": false}""".deserialization()
+
+                            val root = json.shouldBeInstanceOf<JsObject>()
+                            val isActive = root["isActive"]
+                            isActive.shouldBeInstanceOf<JsBoolean.False>()
+                        }
+                    }
+
+                    "the value as the JsNull" {
+                        val json = """{"title": null}""".deserialization()
+
+                        val root = json.shouldBeInstanceOf<JsObject>()
+                        val title = root["title"]
+                        title.shouldBeInstanceOf<JsNull>()
+                    }
+                }
+
+                "the value as the JsArray" {
+                    val json = """["123"]""".deserialization()
+
+                    val root = json.shouldBeInstanceOf<JsArray<JsString>>()
+
+                    root.size shouldBe 1
+                    root[0] shouldBe JsString("123")
+                }
+            }
+        }
+
+        "Serialization" - {
+
+            "success" - {
+
+                "write the value as the array" - {
+                    val json = JsArray(JsString("123"))
+                    val value = json.serialization()
+                    value shouldBe """["123"]"""
+                }
+
+                "write the value as the object" - {
+
+                    "with attribute as the null value" {
+                        val json = JsObject("id" to JsNull)
+                        val value = json.serialization()
+                        value shouldBe """{"id":null}"""
+                    }
+
+                    "with attribute as a string value" {
+                        val json = JsObject("name" to JsString("user-1"))
+                        val value = json.serialization()
+                        value shouldBe """{"name":"user-1"}"""
+                    }
+
+                    "with attribute as a number value" {
+                        val json = JsObject("id" to JsNumber.valueOf(123))
+                        val value = json.serialization()
+                        value shouldBe """{"id":123}"""
+                    }
+
+                    "with attribute as a boolean value" - {
+
+                        "true" {
+                            val json = JsObject("isActive" to JsBoolean.True)
+                            val value = json.serialization()
+                            value shouldBe """{"isActive":true}"""
+                        }
+
+                        "false" {
+                            val json = JsObject("isActive" to JsBoolean.False)
+                            val value = json.serialization()
+                            value shouldBe """{"isActive":false}"""
+                        }
+                    }
+                }
+            }
+        }
     }
 }
