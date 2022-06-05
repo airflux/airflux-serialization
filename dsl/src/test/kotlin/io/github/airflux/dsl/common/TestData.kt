70dd7f23908b89adf8496eab7032f398356274bb
Sambulat <s>
2022-06-05 15:06:47 +0300

    Removed unused code.

--- dsl/src/test/kotlin/io/github/airflux/dsl/common/TestData.kt
+++ dsl/src/test/kotlin/io/github/airflux/dsl/common/TestData.kt
@@ -2,8 +2,4 @@ package io.github.airflux.dsl.common
 
 internal object TestData {
     const val USER_NAME_VALUE: String = "user"
-    const val USER_ROLE_VALUE: String = "employee"
-
-    const val FIRST_PHONE_VALUE: String = "123"
-    const val SECOND_PHONE_VALUE: String = "456"
 }
