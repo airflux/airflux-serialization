fe2a5441f71fe6851171beba17cc02d543376e89
Sambulat <s>
2022-06-21 20:52:22 +0300

    Added the new test for JsPaths ops.

--- core/src/test/kotlin/io/github/airflux/dsl/path/JsPathsOpsTest.kt
+++ core/src/test/kotlin/io/github/airflux/dsl/path/JsPathsOpsTest.kt
@@ -3,6 +3,7 @@ package io.github.airflux.dsl.path
 import io.github.airflux.core.path.JsPath
 import io.github.airflux.core.path.JsPaths
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContainExactly
 import io.kotest.matchers.collections.shouldContainInOrder
 
 internal class JsPathsOpsTest : FreeSpec() {
@@ -15,6 +16,12 @@ internal class JsPathsOpsTest : FreeSpec() {
 
     init {
         "The JsPaths type" - {
+
+            "String#or(String)" - {
+                val paths = "user" or "id"
+                paths.items shouldContainExactly listOf(pathUser, pathId)
+            }
+
             val paths = JsPaths(pathUser)
 
             "JsPaths#or(JsPath) function" - {
