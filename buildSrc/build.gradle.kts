eecb5bf244bc83422cf6591ddb96e5e428b9b5fb
Sambulat <s>
2022-07-19 22:11:43 +0300

    Changed the Kotlin version.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     implementation("org.jacoco:org.jacoco.core:0.8.7")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0")
