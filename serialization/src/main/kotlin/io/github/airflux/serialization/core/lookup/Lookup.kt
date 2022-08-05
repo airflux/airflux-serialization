251405e2ea85d965e14d7f31adae5301027b26cb
Sambulat <s>
2022-08-05 12:01:26 +0300

    Refactored the extension-functions for the Lookup type.

--- serialization/src/main/kotlin/io/github/airflux/serialization/core/lookup/Lookup.kt
+++ serialization/src/main/kotlin/io/github/airflux/serialization/core/lookup/Lookup.kt
@@ -18,6 +18,7 @@ package io.github.airflux.serialization.core.lookup
 
 import io.github.airflux.serialization.core.location.Location
 import io.github.airflux.serialization.core.path.PropertyPath
+import io.github.airflux.serialization.core.path.PropertyPath.Element
 import io.github.airflux.serialization.core.value.ArrayNode
 import io.github.airflux.serialization.core.value.ObjectNode
 import io.github.airflux.serialization.core.value.ValueNode
@@ -27,58 +28,49 @@ public sealed class Lookup {
 
     public abstract val location: Location
 
-    public fun apply(key: String): Lookup = apply(PropertyPath.Element.Key(key))
-    public fun apply(idx: Int): Lookup = apply(PropertyPath.Element.Idx(idx))
-    public abstract fun apply(key: PropertyPath.Element.Key): Lookup
-    public abstract fun apply(idx: PropertyPath.Element.Idx): Lookup
+    public fun apply(key: String): Lookup = apply(Element.Key(key))
+    public fun apply(idx: Int): Lookup = apply(Element.Idx(idx))
+    public abstract fun apply(key: Element.Key): Lookup
+    public abstract fun apply(idx: Element.Idx): Lookup
 
     public data class Defined(override val location: Location, val value: ValueNode) : Lookup() {
-        override fun apply(key: PropertyPath.Element.Key): Lookup = value.lookup(location, key)
-        override fun apply(idx: PropertyPath.Element.Idx): Lookup = value.lookup(location, idx)
+        override fun apply(key: Element.Key): Lookup = value.lookup(location, key)
+        override fun apply(idx: Element.Idx): Lookup = value.lookup(location, idx)
     }
 
     public data class Undefined(override val location: Location) : Lookup() {
-        override fun apply(key: PropertyPath.Element.Key): Lookup = this
-        override fun apply(idx: PropertyPath.Element.Idx): Lookup = this
+        override fun apply(key: Element.Key): Lookup = this
+        override fun apply(idx: Element.Idx): Lookup = this
     }
 }
 
-public fun ValueNode.lookup(location: Location, key: PropertyPath.Element.Key): Lookup =
-    if (this is ObjectNode)
-        this[key]
-            ?.let { Lookup.Defined(location = location.append(key), value = it) }
-            ?: Lookup.Undefined(location = location.append(key))
-    else
-        Lookup.Undefined(location = location.append(key))
+public fun ValueNode.lookup(location: Location, key: Element.Key): Lookup =
+    this.lookup(key)
+        ?.let { Lookup.Defined(location = location.append(key), value = it) }
+        ?: Lookup.Undefined(location = location.append(key))
 
-public fun ValueNode.lookup(location: Location, idx: PropertyPath.Element.Idx): Lookup =
-    if (this is ArrayNode<*>)
-        this[idx]
-            ?.let { Lookup.Defined(location = location.append(idx), value = it) }
-            ?: Lookup.Undefined(location = location.append(idx))
-    else
-        Lookup.Undefined(location = location.append(idx))
+public fun ValueNode.lookup(location: Location, idx: Element.Idx): Lookup =
+    this.lookup(idx)
+        ?.let { Lookup.Defined(location = location.append(idx), value = it) }
+        ?: Lookup.Undefined(location = location.append(idx))
 
-public fun ValueNode.lookup(location: Location, path: PropertyPath): Lookup {
+public fun ValueNode.lookup(location: Location, path: PropertyPath): Lookup =
+    this.lookup(path)
+        ?.let { Lookup.Defined(location = location.append(path), value = it) }
+        ?: Lookup.Undefined(location = location.append(path))
 
-    tailrec fun lookup(location: Location, path: PropertyPath, idxElement: Int, value: ValueNode): Lookup {
-        if (idxElement == path.elements.size) return Lookup.Defined(location, value)
+internal fun ValueNode.lookup(key: Element.Key): ValueNode? = if (this is ObjectNode) this[key] else null
+
+internal fun ValueNode.lookup(idx: Element.Idx): ValueNode? = if (this is ArrayNode<*>) this[idx] else null
+
+internal fun ValueNode.lookup(path: PropertyPath): ValueNode? {
+    tailrec fun lookup(path: PropertyPath, idxElement: Int, value: ValueNode?): ValueNode? {
+        if (value == null || idxElement == path.elements.size) return value
         return when (val element = path.elements[idxElement]) {
-            is PropertyPath.Element.Key -> if (value is ObjectNode) {
-                val currentValue = value[element]
-                    ?: return Lookup.Undefined(location.append(path.elements.subList(idxElement, path.elements.size)))
-                lookup(location.append(element), path, idxElement + 1, currentValue)
-            } else
-                Lookup.Undefined(location = location.append(path.elements.subList(idxElement, path.elements.size)))
-
-            is PropertyPath.Element.Idx -> if (value is ArrayNode<*>) {
-                val currentValue = value[element]
-                    ?: return Lookup.Undefined(location.append(path.elements.subList(idxElement, path.elements.size)))
-                lookup(location.append(element), path, idxElement + 1, currentValue)
-            } else
-                Lookup.Undefined(location = location.append(path.elements.subList(idxElement, path.elements.size)))
+            is Element.Key -> if (value is ObjectNode) lookup(path, idxElement + 1, value[element]) else null
+            is Element.Idx -> if (value is ArrayNode<*>) lookup(path, idxElement + 1, value[element]) else null
         }
     }
 
-    return lookup(location, path, 0, this)
+    return lookup(path, 0, this)
 }
