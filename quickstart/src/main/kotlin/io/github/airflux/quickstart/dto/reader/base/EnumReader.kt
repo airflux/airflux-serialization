8d3b48181fa6b4079d5d23729a5ebc806140b56b
Sambulat <s>
2022-08-12 21:58:20 +0300

    The example of the enum reader was changed.

--- quickstart/src/main/kotlin/io/github/airflux/quickstart/dto/reader/base/EnumReader.kt
+++ quickstart/src/main/kotlin/io/github/airflux/quickstart/dto/reader/base/EnumReader.kt
@@ -3,24 +3,17 @@ package io.github.airflux.quickstart.dto.reader.base
 import io.github.airflux.quickstart.json.error.JsonErrors
 import io.github.airflux.serialization.core.reader.Reader
 import io.github.airflux.serialization.core.reader.result.ReaderResult
+import io.github.airflux.serialization.core.reader.result.success
 
 inline fun <reified T : Enum<T>> Reader<String>.asEnum(): Reader<T> =
-    Reader { context, location, source ->
-        read(context, location, source)
-            .asEnum()
-    }
-
-inline fun <reified T : Enum<T>> ReaderResult<String>.asEnum(): ReaderResult<T> =
-    this.asEnum(enumValues()) { text -> enumValueOf(text.uppercase()) }
-
-fun <T : Enum<T>> ReaderResult<String>.asEnum(allowable: Array<T>, transform: (String) -> T): ReaderResult<T> =
-    flatMap { location, text ->
+    flatMap { _, location, value ->
         try {
-            ReaderResult.Success(location, transform(text))
+            enumValueOf<T>(value.uppercase()).success(location)
         } catch (ignored: Exception) {
+            val allowable = enumValues<T>()
             ReaderResult.Failure(
                 location = location,
-                error = JsonErrors.EnumCast(actual = text, expected = allowable.joinToString())
+                error = JsonErrors.EnumCast(actual = value, expected = allowable.joinToString())
             )
         }
     }
