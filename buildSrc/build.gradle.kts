c36ede4cef4599f474bb129095cb12899f4db522
Sambulat <s>
2022-11-30 17:15:28 +0200

    Changed the version of Detekt to 1.22.0

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,6 +19,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     implementation("org.jacoco:org.jacoco.core:0.8.8")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.21.0")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.22.0")
     implementation("org.jetbrains.kotlinx:kover:0.5.1")
 }
