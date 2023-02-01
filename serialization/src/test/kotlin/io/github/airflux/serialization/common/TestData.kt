b3aae58b21c7d7aaa5267e5afa17f046b7e335ab
Sambulat <s>
2023-02-01 19:45:05 +0200

    Removed the unused code.

--- serialization/src/test/kotlin/io/github/airflux/serialization/common/TestData.kt
+++ serialization/src/test/kotlin/io/github/airflux/serialization/common/TestData.kt
@@ -17,8 +17,6 @@
 package io.github.airflux.serialization.common
 
 internal object TestData {
-    const val USER_NAME_VALUE: String = "user"
     const val FIRST_PHONE_VALUE: String = "123"
     const val SECOND_PHONE_VALUE: String = "456"
-    const val DEFAULT_VALUE: String = "Default value"
 }
