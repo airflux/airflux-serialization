8ab62989c974fc1ebfc7c01cbfe9c4a4b31bb4b7
mx.smblt <mx.smblt@gmail.com>
2023-05-25 12:43:59 +0300

    The common types for the tests were extracted into the module.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,9 @@ project(":airflux-serialization-dsl").projectDir = file("./serialization/dsl")
 include(":airflux-serialization-std")
 project(":airflux-serialization-std").projectDir = file("./serialization/std")
 
+include(":airflux-serialization-test-core")
+project(":airflux-serialization-test-core").projectDir = file("./serialization/test/core")
+
 include(":airflux-jackson-parser")
 project(":airflux-jackson-parser").projectDir = file("./parser/jackson")
 
