2d3023324c6139c8015f88e89ab445be4ff413dd
mx.smblt <mx.smblt@gmail.com>
2023-07-06 23:27:41 +0300

    Changed the version of the binary-compatibility-validator plugin to 0.13.2

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,5 +22,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.2")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
     implementation("com.github.jk1:gradle-license-report:2.2")
-    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.13.1")
+    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.13.2")
 }
