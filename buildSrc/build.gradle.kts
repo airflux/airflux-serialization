f2e5b7a6750d15f74e47af3bef5044a63e20e861
mx.smblt <mx.smblt@gmail.com>
2024-04-21 16:26:33 +0300

    Changed the versions of dependencies.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
+        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     }
 }
 
@@ -17,10 +17,10 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.21")
-    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.0")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.2")
-    implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.0")
-    implementation("com.github.jk1:gradle-license-report:2.2")
-    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.13.2")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.6")
+    implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
+    implementation("com.github.jk1:gradle-license-report:2.5")
+    implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.14.0")
 }
