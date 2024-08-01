b29680fb54e68d55a21623a7661bf05929b80912
mx.smblt <mx.smblt@gmail.com>
2024-08-01 22:38:59 +0300

    Changed the version of the `Kover` plugin to 0.8.3

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
     implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.23.6")
-    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.7.6")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:0.8.3")
     implementation("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
     implementation("com.github.jk1:gradle-license-report:2.5")
     implementation("org.jetbrains.kotlinx.binary-compatibility-validator:org.jetbrains.kotlinx.binary-compatibility-validator.gradle.plugin:0.16.2")
--- buildSrc/src/main/kotlin/kover-merge-convention.gradle.kts
+++ buildSrc/src/main/kotlin/kover-merge-convention.gradle.kts
@@ -2,20 +2,22 @@ plugins {
     id("org.jetbrains.kotlinx.kover")
 }
 
-koverReport {
-    filters {
-        includes {
-            classes("io.github.airflux.*")
+kover {
+    reports {
+        filters {
+            includes {
+                classes("io.github.airflux.*")
+            }
         }
-    }
+        total {
+            val baseDir = "reports/jacoco/test"
+            xml {
+                xmlFile.set(layout.buildDirectory.file("$baseDir/jacocoTestReport.xml"))
+            }
 
-    defaults {
-        val baseDir = "reports/jacoco/test"
-        xml {
-            setReportFile(layout.buildDirectory.file("$baseDir/jacocoTestReport.xml"))
-        }
-        html {
-            setReportDir(layout.buildDirectory.dir("$baseDir/html"))
+            html {
+                htmlDir.set(layout.buildDirectory.dir("$baseDir/html"))
+            }
         }
     }
 }
